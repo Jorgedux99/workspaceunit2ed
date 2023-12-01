d764adfa5e5084e96965d02249509895a3587878
Jorgedux99 <jirgeserramo9@gmail.com>
2023-12-01 12:45:31 +0100

    Version 1.0.1 01/12/23

---
+++ Prueba1/src/prueba1/Cambio1.java
@@ -0,0 +1,11 @@
+package prueba1;
+
+public class Cambio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Pruebas GIT, Clase Cambio1");
+
+	}
+
+}
---
+++ Prueba1/src/prueba1/Principal1.java
@@ -0,0 +1,14 @@
+package prueba1;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Pruebas git. Se añade Cambio1");
+		
+	}
+
+}
