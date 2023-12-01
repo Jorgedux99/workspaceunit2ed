86dbb5858707d11f4260cbb2fca252cc90dea0eb
Jorgedux99 <jirgeserramo9@gmail.com>
2023-12-01 12:47:13 +0100

    Version 1.0.2

--- Prueba1/src/prueba1/Cambio1.java
+++ Prueba1/src/prueba1/Cambio1.java
@@ -5,7 +5,7 @@ public class Cambio1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Pruebas GIT, Clase Cambio1");
-
+		System.out.println("Pruebas GIT. Clase Cambio1. Añadimos cambio para comparar");
 	}
 
 }
